9fb3b065f9895bb3f53a1f5d1ba47d4eaede65cb
Viktor Roth <viktor.roth.98@icloud.com>
2019-04-21 22:31:30 +1000

    Updates

---
+++ precache-manifest.2459ef8e912f987c141be08dfe4d2cfa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "136e3483cd45e80996e7",
+    "url": "/robofriends/static/css/main.2427188e.chunk.css"
+  },
+  {
+    "revision": "136e3483cd45e80996e7",
+    "url": "/robofriends/static/js/main.38870f55.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "daf773c239e6d5d6d5bd",
+    "url": "/robofriends/static/css/2.85e47834.chunk.css"
+  },
+  {
+    "revision": "daf773c239e6d5d6d5bd",
+    "url": "/robofriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "9d94bdd06c0d2afbfeb8165cd7b6ccd8",
+    "url": "/robofriends/index.html"
+  }
+];
